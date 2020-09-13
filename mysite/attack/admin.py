ec440cb0d8a7d79c4e0e3fefecbcf311c836a4ab
antonio-tandoi <63054617+antonio-tandoi@users.noreply.github.com>
2020-09-13 14:53:15 +0200

    Add files via upload

--- mysite/attack/admin.py
+++ mysite/attack/admin.py
@@ -16,28 +16,90 @@ admin.site.index_title = "Dashboard"
 
 class AttacksAdmin(AutocompleteFilterMixin, admin.ModelAdmin):
 	
+	'''
+	1 - 3  : Attivo/Passivo
+	4 - 7  : TCP/IP Layer - 19: None
+	8 - 10 : Exploit
+	11 - 18: Tipology
+	20 - 22: CIA Triad
+	23 - 25: Difficoltà
+	'''
+	
 	def display_value_1(self,object):
-		return ','.join(v.value for v in object.valori_categoria.all()[:1])
+		mylist = []
+		for v in object.valori_categoria.all():
+			mylist.append(v)
+		for element in mylist:
+			if element.id_value <= 3:
+				return element.value	
 	display_value_1.short_description = 'Attivo/Passivo'
-	
+  	
 	def display_value_2(self,object):
-		return ','.join(v.value for v in object.valori_categoria.all()[1:2])
+		mylist = []
+		for v in object.valori_categoria.all():
+			mylist.append(v)
+		for element in mylist:
+			if (element.id_value >= 4 and element.id_value <= 7) or element.id_value == 19:
+				return element.value
 	display_value_2.short_description = 'TCP/IP Layer'
-	
+   	
 	def display_value_3(self,object):
-		return ','.join(v.value for v in object.valori_categoria.all()[2:3])
+		mylist = []
+		for v in object.valori_categoria.all():
+			mylist.append(v)
+		for element in mylist:
+			if (element.id_value >= 8 and element.id_value <= 10):
+				return element.value
 	display_value_3.short_description = 'Exploit'
-	
+    	
 	def display_value_4(self,object):
-		return ','.join(v.value for v in object.valori_categoria.all()[3:4])
+		mylist = []
+		for v in object.valori_categoria.all():
+			mylist.append(v)
+		for element in mylist:
+			if (element.id_value >= 11 and element.id_value <= 18):
+				return element.value
+		#return ','.join(v.value for v in object.valori_categoria.all()[3:4])
 	display_value_4.short_description = 'Tipologia'
 	
+	def display_value_5(self,object):
+		mylist = []
+		for v in object.valori_categoria.all():
+			mylist.append(v)
+		for element in mylist:
+			if (element.id_value >= 20 and element.id_value <= 22):
+				return element.value
+		#return ','.join(v.value for v in object.valori_categoria.all()[3:4])
+	display_value_5.short_description = 'CIA triad'
 	
-	list_display = ('name', 'display_value_3', 
-				'display_value_4')
+	def display_value_6(self,object):
+		mylist = []
+		for v in object.valori_categoria.all():
+			mylist.append(v)
+		for element in mylist:
+			if (element.id_value >= 23 and element.id_value <= 25):
+				return element.value
+		#return ','.join(v.value for v in object.valori_categoria.all()[3:4])
+	display_value_6.short_description = 'Difficoltà'
+	
+	def display_correlation(self, object):
+		mylist = []
+		for a in object.correlazione.all():
+			mylist.append(a)
+		if len(mylist) == 0:
+			return "None"
+		return "\n".join([a.name for a in object.correlazione.filter()])
+	display_correlation.short_description = "Correlazione"
+				
+	'''
+	Di default, il link per modificare l'oggetto è posto sul primo elemento di list_display;
+	Usare list_display_links per selezionare quali field devono avere il link
+	'''
+	list_display = ('name', 'display_value_2', 'display_value_4',)
 	list_filter = (('valori_categoria', AutocompleteListFilter),)
-	search_fields = ['name', 'atk_description']
-	fields = ['name', 'atk_description', 'valori_categoria','correlazione']
+	search_fields = ['name', 'atk_description', 'otherNames']
+	fields = ['name', 'atk_description', 'otherNames', 'valori_categoria', 'correlazione', ]
+	filter_horizontal = ('correlazione',)
 	list_per_page = 30
 	show_full_result_count = True
 	#raw_id_fields = ('correlazione',)
@@ -49,12 +111,16 @@ admin.site.register(Attacks, AttacksAdmin)
 #######################################################################################
 
 class ValuesAdmin(AutocompleteFilterMixin, admin.ModelAdmin):
-	list_display = ('value', 'descrizione')
-	list_filter = (('id_cat', AutocompleteListFilter),)
+	list_display = ('value', 'descrizione', 'categoryName')
+	list_filter = (('id_category', AutocompleteListFilter),)
 	search_fields = ['value'] 
-	fields = ['value','descrizione', 'id_cat']
+	fields = ('value', 'id_category','descrizione')
 	
 	icon_name = 'settings_input_component' 
+	
+	def categoryName(self, instance):
+		return instance.id_category.category
+	categoryName.short_description = "Categoria"
 	
 admin.site.register(Values, ValuesAdmin)
 
@@ -63,6 +129,7 @@ admin.site.register(Values, ValuesAdmin)
 class CategoriesAdmin(admin.ModelAdmin):
 	list_display = ('category', 'cat_description')
 	search_fields = ['category', 'cat_description']
+	field = ['category', 'cat_description',]
 	
 	icon_name = 'receipt'
 	
--- mysite/attack/models.py
+++ mysite/attack/models.py
@@ -1,36 +1,36 @@
 from django.db import models
-from sortedm2m.fields import SortedManyToManyField
 
 class Categories(models.Model):
-    id_category = models.AutoField(db_column='ID_Category', primary_key=True)  
-    category = models.CharField(db_column='Category', max_length=100, verbose_name = "Categoria")  
-    cat_description = models.TextField(db_column='CAT_Description', 
-                                       max_length=500, verbose_name = "Descrizione")  
-
+    id = models.AutoField(db_column='ID', primary_key=True)  
+    category = models.CharField(db_column='Category', max_length=100, verbose_name = "Categoria", 
+                                blank=True, null=True)  
+    cat_description = models.TextField(db_column='CAT_DESCRIPTION', blank=True, null=True,
+                                       max_length=100, verbose_name = "Descrizione") 
+    
     def __str__(self):
         return self.category
         
     class Meta:
-        managed = False
         verbose_name_plural = "Categorie"
         ordering = ['category']
         db_table = 'Categories'
 
 #######################################################################################
 
-class Values(models.Model):
-    id_value = models.IntegerField(db_column='ID_Value', primary_key=True)  
-    id_cat = models.ForeignKey(Categories, models.DO_NOTHING, db_column='ID_Cat',
-                               verbose_name = "Categoria")
-    value = models.CharField(db_column='Value', max_length=100, verbose_name = "Valore")
-    descrizione = models.TextField(db_column='Descrizione', max_length=500, blank=True, null=True,
-                                   verbose_name = "Descrizione")  
+class Values(models.Model):    
+    id_value = models.AutoField(db_column='ID_Value', primary_key=True)
+    value = models.CharField(db_column='Value', max_length=100, blank=True, null=True, 
+                             verbose_name = "Valore")  
+    descrizione = models.TextField(db_column='Descrizione', max_length=500, blank=True, null=True, verbose_name = "Descrizione")
+    id_category = models.ForeignKey(Categories, models.DO_NOTHING, db_column='ID_CATEGORY', 
+                                    blank=True, null=True, verbose_name = "Categoria") 
+
+  
 
     def __str__(self):
         return self.value
     
     class Meta:
-        managed = False
         ordering = ['id_value']
         verbose_name_plural = "Valori"
         db_table = 'Values'
@@ -38,137 +38,25 @@ class Values(models.Model):
 #######################################################################################
 
 class Attacks(models.Model):
-    name = models.CharField(db_column='Name', max_length=100, verbose_name = "Nome")  
-    atk_description = models.TextField(db_column='ATK_Description', max_length=500, 
-                                       verbose_name = "Descrizione") 
-    id_atk = models.AutoField(db_column='ID_ATK', primary_key=True) 
+    id = models.AutoField(db_column='ID', primary_key=True)
+    name = models.CharField(db_column='Name', max_length=100, blank=True, null=True, 
+                            verbose_name = "Nome") 
+    atk_description = models.TextField(db_column='ATK_Description', max_length=500, blank=True, null=True,
+                                       verbose_name = "Descrizione")
+    otherNames = models.TextField(db_column='OtherNames', max_length=100, blank=True, null=True,
+                                       verbose_name = "Nomi Alternativi")
     valori_categoria = models.ManyToManyField(Values)
-    #correlazione = SortedManyToManyField('Attacks')
-    correlazione = models.ManyToManyField('Attacks')
+    correlazione = models.ManyToManyField('Attacks', blank = True) 
+
+    class Meta:
+        verbose_name_plural = "Attacchi"
+        db_table = 'Attacks' 
+        ordering = ['name']  
     
     def __str__(self):
         return self.name
-
     
-#     def get_absolute_url(self):
-#         return reverse('model-detail-view', args=[str(self.id)])
-
-
-    class Meta:
-        ordering = ['name']
-        verbose_name_plural = "Attacchi"
-        db_table = 'Attacks'
-
-
-#######################################################################################
-#######################################################################################
 #######################################################################################
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
 
 
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
